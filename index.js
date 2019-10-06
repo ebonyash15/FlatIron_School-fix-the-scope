a644cccc317e0fd8a62d25379f161be81d0b9a4c
ebonyash15 <56212844+ebonyash15@users.noreply.github.com>
2019-10-06 18:18:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var animal = 'dog'
+function myAnimal() {return animal}
+function yourAnimal() {var animal='cat'; return animal}
+function add2(n) {const two=2; return n + two}
+var funkyFunction = function() {return function() {return "FUNKY!"}}
+var theFunk = funkyFunction()();
+theFunk()
